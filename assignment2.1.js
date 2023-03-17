bf716d85d1da4eb5cebd74aac272cec1e827b5fc
Prashant <95129447+prashantadhav@users.noreply.github.com>
2023-03-17 23:30:41 +0530

    Assignment2.1

---
+++ assignment2.1.js
@@ -0,0 +1,9 @@
+// Define a function called "divideByTwo" that takes one parameter: "num"
+function divideByTwo(num) {
+    // Divide the input number by two and return the result
+    return num / 2;
+  }
+  
+  // Test the function by calling it with a sample input
+  console.log(divideByTwo(10)); // Output: 5
+  
